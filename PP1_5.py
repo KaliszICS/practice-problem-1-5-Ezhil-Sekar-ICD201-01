d58b08d66d29675a69ecf1d7044ec360606f71e2
Ezhil-Sekar-ICD201-01 <166552821+Ezhil-Sekar-ICD201-01@users.noreply.github.com>
2024-09-23 13:54:10 +0000

    Ezhil

--- PP1_5.py
+++ PP1_5.py
@@ -1,18 +1,30 @@
 
 def q1():
   #Write Assignment code here
-
+  user = int(input("Input an integer: "))
+  a = user + 3  
+  print(a)
 def q2():
   #Write Assignment code here
+  user1 = int(input("Input a number: "))
+  a = float(str(user1) + "4")+2
+  print(a)
 
 def q3():
   #Write Assignment code here
-
+  user2 = int(input("Input a radius: "))
+  a = float((user2**2)*3.14)
+  print(a)
 def q4():
   #Write Assignment code here
-
+  user3 = float(input("Input a number: "))
+  a = int(user3 * 12)
+  print(a)
 def q5():
   #Write Assignment code here
+  user4 = int(input("Input an integer: "))
+  result = user4 + 5
+  print(f'Your number + 5 is {result}')
 
 #Comment this code out when running tests
 #Do not comment this out when running your program normally
